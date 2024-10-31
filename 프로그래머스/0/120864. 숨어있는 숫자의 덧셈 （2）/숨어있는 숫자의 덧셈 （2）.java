702cab3010a4635418a7d2e98e09d7f94c6d1afe
park-minji14 <163144029+park-minji14@users.noreply.github.com>
2024-11-01 01:24:25 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.14 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : my_string.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sb.append(c);
+            } else if (sb.length() > 0) {
+                answer += Integer.parseInt(sb.toString());
+                sb.setLength(0);
+            }
+        }
+        
+        if (sb.length() > 0) {
+            answer += Integer.parseInt(sb.toString());
+        }
+        
+        return answer;
+    }
+}
